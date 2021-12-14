3bea15e90c49362bf8edf9a17200a5857b7d9868
Pooya Parsa <pyapar@gmail.com>
2021-12-14 21:46:25 +0100

    fix: remove all node.js imports

--- build.config.ts
+++ build.config.ts
@@ -1,4 +1,7 @@
 import { defineBuildConfig } from 'unbuild'
+import { resolve } from 'path'
+
+const mocksFile = resolve(__dirname, 'src/mocks.ts')
 
 export default defineBuildConfig({
   declaration: true,
@@ -12,11 +15,28 @@ export default defineBuildConfig({
         'node:url': 'url',
         'legacyRequire(': 'require(',
         'require("espree")': '{}',
-        'require("acorn-walk")': '{}'
+        'require("acorn-walk")': '{}',
+        'createRequire(import.meta.url)': 'null'
       }
     }
   },
+  // TODO: use unenv
+  alias: {
+    url: mocksFile,
+    fs: mocksFile,
+    os: mocksFile,
+    tty: mocksFile,
+    path: mocksFile,
+    module: mocksFile,
+    chalk: mocksFile
+  },
   entries: [
     './src/html-validate'
-  ]
+  ],
+  hooks: {
+    'build:prepare'(ctx) {
+      // Disable node built-ins
+      ctx.options.externals = []
+    }
+  }
 })
